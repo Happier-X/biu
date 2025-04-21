03dd5c609b46cef5271f3aa86c1cfe38caea828e
Happier <zhf5256@qq.com>
2025-04-21 20:05:09 +0800

    feat: 增加头像

--- src/renderer/src/components/Search.vue
+++ src/renderer/src/components/Search.vue
@@ -1,10 +1,21 @@
 <template>
-  <input
-    type="text"
-    placeholder="你好..."
-    autofocus
-    @click="fun"
-    class="w-full h-[65px] input input-ghost input-xl focus:outline-none"
-  />
+  <div class="flex w-full h-[65px]">
+    <input
+      type="text"
+      placeholder="你好..."
+      autofocus
+      class="w-full h-full input input-ghost input-xl focus:outline-none select-none"
+      @click="fun"
+    />
+    <div class="h-full avatar p-[0.75rem] pl-0 select-none">
+      <div class="h-full rounded-full">
+        <img
+          draggable="false"
+          class="pointer-events-none"
+          src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts"></script>
